93193cdb420bfdd22664996377e8f02a3258fac2
bunisun <sunwei0130@gmail.com>
2015-05-11 11:36:15 +0800

    server connection UI added

--- WechatSmuggle/Window1.xaml.cs
+++ WechatSmuggle/Window1.xaml.cs
@@ -23,5 +23,50 @@ namespace WechatSmuggle
         {
             InitializeComponent();
         }
+        String hostaddr, username, dbpswd, dbname;
+        String connMsgHead = "测试链接：\n";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //测试链接按钮点击
+            textbx_connMsg.Text = "";
+            hostaddr = txbx_serverAddr.Text;
+            username = txbx_username.Text;
+            dbpswd = txbx_pswd.Text;
+            dbname = txbx_dbname.Text;
+
+            //检测服务器地址，用户名和密码是否输入
+            if(hostaddr.Length*username.Length*dbpswd.Length==0){
+                StringBuilder invalidInputsb = new StringBuilder(connMsgHead).Append("请输入数据库服务器地址，用户名和密码");
+                textbx_connMsg.Text = invalidInputsb.ToString();
+
+            }
+        }
+
+        private void textbx_connMsg_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //server address textbox
+        }
+
+        private void txbx_username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txbx_pswd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txbx_dbname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
     }
 }
